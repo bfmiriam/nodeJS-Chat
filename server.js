8d06c205dc8ef63031dc87371dbc20570f7933f6
EC2 Default User <ec2-user@ip-172-31-10-230.us-east-2.compute.internal>
2019-03-27 22:01:05 +0000

    envio de notificaciones a usuarios

---
+++ server.js
@@ -0,0 +1,78 @@
+// imports
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+var request = require('request');
+//var request2 = require('request');
+
+
+// inicializamos la conexion con firebase
+// necesitamos json con las credenciales 
+var admin = require('firebase-admin');
+var serviceAccount = require('./dbfirebase.json');
+admin.initializeApp({
+
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://telegram-44928.firebaseio.com'
+
+});
+
+var db = admin.database();
+var ref = db.ref();
+
+
+var API_KEY = "API_KEY";
+
+
+
+function listenForNotificationRequests() {
+  var requests = ref.child('usuarios');
+//  var requests2 = ref.child('mensajes');
+
+
+  requests.on('child_added', function(requestSnapshot) {
+    var request = requestSnapshot.val();
+
+    sendNotificationToUser(
+      request.usuario, 
+      request.token, 
+   
+      function() {
+        requestSnapshot.ref.remove();
+      }
+   );
+  }, function(error) {
+    console.error(error);
+  });
+  
+
+  
+
+  
+};
+
+function sendNotificationToUser(usuario, token) {
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type' :' application/json',
+      'Authorization': 'key='+API_KEY
+    },
+    body: JSON.stringify({
+      notification: {
+        title: "tienes un nuevo mensaje",
+        body: usuario+" te ha escrito"
+      },
+      to : token
+    })
+  
+  });
+}
+
+
+
+
+
+listenForNotificationRequests();
+
